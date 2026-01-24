c217f84438a6740afc8bf6077e923e39a47dca9b
HiLeeS <azs8017@naver.com>
2026-01-24 14:10:05 +0900

    [Feat] 프로필 이미지 변경 api 추가

---
+++ src/main/java/com/my4cut/global/image/ImageConstants.java
@@ -0,0 +1,9 @@
+package com.my4cut.global.image;
+
+public final class ImageConstants {
+
+    private ImageConstants() {}
+
+    public static final String DEFAULT_PROFILE_IMAGE_URL =
+            "/uploads/profile/default.png";
+}

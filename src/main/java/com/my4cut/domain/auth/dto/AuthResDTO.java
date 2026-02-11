00920cfd39c43b5680e507c240be2738ffe34b19
HiLeeS <azs8017@naver.com>
2026-02-11 17:08:05 +0900

    [Feat] 이메일 중복 체크 구현

--- src/main/java/com/my4cut/domain/auth/dto/AuthResDTO.java
+++ src/main/java/com/my4cut/domain/auth/dto/AuthResDTO.java
@@ -2,6 +2,11 @@ package com.my4cut.domain.auth.dto;
 
 public class AuthResDTO {
 
+    public record CheckEmailResDto(
+            String email,
+            Boolean duplicated
+    ) {}
+
     // 카카오 /v2/user/me 응답 중 필요한 것만
     public record KakaoUserResDto(
             Long id
